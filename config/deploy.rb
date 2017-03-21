89b5d0b1ff0e09bcbff4e89cb14e83e8982bae0a
lanqiu1986 <lisuji1986@163.com>
2017-03-21 14:51:44 +0800

    add capistrano file changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, "/home/ror01/blog"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-set :linked_files, fetch (:linked_files, []).push('config/database.yml','config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml','config/secrets.yml')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
